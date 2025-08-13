7aaffedf866257349bfdd0de0a1e2667ce1d7590
Nirman Patel <144795287+NirmanPatel036@users.noreply.github.com>
2025-08-13 17:04:28 +0530

    Non-null assertion errors fixed for ESLint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import Link from "next/link";
+import Image from 'next/image';
 import { useUser } from "@clerk/nextjs";
 import { SignInButton } from "@clerk/nextjs";
 import React, { useRef, useEffect, useState } from 'react';
